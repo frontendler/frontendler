9a1b77cf0062008685783410f755b7933bf550c4
Daniel Beff <danielbeff@MacBook-Pro-de-Daniel.local>
2015-08-12 22:33:47 -0300

    add plumber in the sass taks, add clearfix mixin and include on grid model

--- gulpfile.js
+++ gulpfile.js
@@ -49,6 +49,7 @@ var AUTOPREFIXER_BROWSERS = [
 
 gulp.task('styles', function () {
   gulp.src( app + '/' + styles + '/**/*.scss')
+    .pipe(plugins.plumber())
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass().on('error', plugins.sass.logError))
     .pipe(plugins.autoprefixer(AUTOPREFIXER_BROWSERS))
