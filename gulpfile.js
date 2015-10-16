567ae93e9350dfe752556908dca0ab0fd3ae9d8a
Daniel Beff <danielbeff@MacBook-Pro-de-Daniel.local>
2015-10-15 22:57:54 -0300

    update package dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,14 @@
-'use strict';
-
-
 //-------------------------------------------------------------------
 // VARS
 //-------------------------------------------------------------------
 
 // plugins
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var del = require('del');
-var runSequence = require('run-sequence');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var pagespeed = require('psi');
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')(),
+	del = require('del'),
+	runSequence = require('run-sequence'),
+	browserSync = require('browser-sync'),
+	pagespeed = require('psi');
 
 
 //-------------------------------------------------------------------
@@ -32,51 +28,57 @@ var fonts = 'assets/fonts';
 var template = 'template';
 
 var AUTOPREFIXER_BROWSERS = [
-  'ie >= 9',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+	'ie >= 9',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4',
+	'bb >= 10',
 ];
 
 //-------------------------------------------------------------------
 // TASKS
 //-------------------------------------------------------------------
 
-gulp.task('styles', function () {
-  gulp.src( app + '/' + styles + '/**/*.scss')
-    .pipe(plugins.plumber())
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass().on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest( dev + '/' + styles))
-    .pipe(plugins.size({title: 'styles'}));
+gulp.task('styles', function() {
+	gulp.src(app + '/' + styles + '/**/*.scss')
+		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass().on('error', plugins.sass.logError))
+		.pipe(plugins.autoprefixer(AUTOPREFIXER_BROWSERS))
+		.pipe(plugins.sourcemaps.write())
+		.pipe(gulp.dest(dev + '/' + styles))
+		.pipe(browserSync.stream())
+		.pipe(plugins.size({
+			title: 'styles',
+		}));
+
 });
 
 gulp.task('scripts', function() {
 	return gulp.src(app + '/' + scripts + '/**/*.js')
 		.pipe(plugins.plumber())
-	    .pipe(plugins.jshint())
-	    .pipe(plugins.jshint.reporter('jshint-stylish'))
-	    .pipe(plugins.if(!browserSync.active, plugins.jshint.reporter('fail')));
+		.pipe(plugins.eslint())
+		.pipe(plugins.eslint.format())
+		.pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('templates', function() {
 	var YOUR_LOCALS = {};
-	return gulp.src([ app+ '/' + template + '/**/*.jade', '!' +app + '/' + template + '/**/_*.jade'])
+	return gulp.src([app + '/' + template + '/**/*.jade', '!' + app + '/' + template + '/**/_*.jade'])
 		.pipe(plugins.plumber())
 		.pipe(plugins.jade({
 			locals: YOUR_LOCALS,
 			pretty: true,
-			basedir: app+ '/' + template
+			basedir: app + '/' + template
 		}))
 		.pipe(gulp.dest(dev + '/'))
-		.pipe(plugins.size({title: 'template'}));
+		.pipe(plugins.size({
+			title: 'template'
+		}));
 });
 
 gulp.task('images', function() {
@@ -91,18 +93,22 @@ gulp.task('images', function() {
 		.pipe(plugins.size());
 });
 
-gulp.task('html',function() {
-	var assets = plugins.useref.assets({searchPath: '{' + dev + ',' + app + '}' });
+gulp.task('html', function() {
+	var assets = plugins.useref.assets({
+		searchPath: '{' + dev + ',' + app + '}'
+	});
 	return gulp.src([dev + '/**/*.html'])
 		.pipe(plugins.plumber())
 		.pipe(assets)
 		.pipe(plugins.if('*.js', plugins.uglify()))
 		.pipe(plugins.if('*.css', plugins.csso()))
 		.pipe(assets.restore())
-    	.pipe(plugins.useref())
-        .pipe(plugins.if('*.html', plugins.minifyHtml()))
+		.pipe(plugins.useref())
+		.pipe(plugins.if('*.html', plugins.minifyHtml()))
 		.pipe(gulp.dest(prod + '/'))
-		.pipe(plugins.size({title: 'html'}));
+		.pipe(plugins.size({
+			title: 'html'
+		}));
 });
 
 //-------------------------------------------------------------------
@@ -111,28 +117,28 @@ gulp.task('html',function() {
 
 
 gulp.task('clean:dev', del.bind(null, [dev]));
-gulp.task('serve:dev', function () {
+gulp.task('serve:dev', function() {
 
 	browserSync({
-		logFileChanges:true,
-        notify: true,
-        //tunnel: 'frontendler',
-        logPrefix: 'Frontendler',
+		//tunnel: 'frontendler',
+		logConnections: true,
+		logFileChanges: true,
+		logPrefix: 'Frontendler',
 		server: {
 			baseDir: [dev,app]
 		}
 	});
 
-	gulp.watch( [app + '/' + template + '/**/*.jade'], ['templates',reload]);
-	gulp.watch( [app + '/' + scripts + '/**/*.js'], ['scripts',reload]);
-    gulp.watch( [app + '/' + styles + '/**/*.scss'], ['styles',reload]);
-    gulp.watch( [app + '/' + fonts + '**/*'], ['fonts',reload]);
-	gulp.watch( [app + '/' + images + '/**/*'], reload);
+	gulp.watch([app + '/' + template + '/**/*.jade'], ['templates', browserSync.reload]);
+	gulp.watch([app + '/' + scripts + '/**/*.js'], ['scripts', browserSync.reload]);
+	gulp.watch([app + '/' + styles + '/**/*.scss'], ['styles']);
+	gulp.watch([app + '/' + fonts + '**/*'], ['fonts', browserSync.reload]);
+	gulp.watch([app + '/' + images + '/**/*'], browserSync.reload);
 
 });
 
-gulp.task('watch',['clean:dev'],function(cb) {
-	runSequence(['styles','templates'],'serve:dev',cb);
+gulp.task('watch', ['clean:dev'], function(cb) {
+	runSequence(['styles', 'templates'], 'serve:dev', cb);
 });
 
 //-------------------------------------------------------------------
@@ -141,23 +147,25 @@ gulp.task('watch',['clean:dev'],function(cb) {
 
 gulp.task('clean:prod', del.bind(null, [prod]));
 gulp.task('copy:prod', function() {
-    gulp.src([app + '/' + fonts + '/**/*.{eot,svg,ttf,woff}'])
-        .pipe(gulp.dest(prod + '/' + fonts));
-    gulp.src([app + '/*.*','node_modules/apache-server-configs/dist/.htaccess'],{ dot: true })
-        .pipe(gulp.dest(prod));
+	gulp.src([app + '/' + fonts + '/**/*.{eot,svg,ttf,woff}'])
+		.pipe(gulp.dest(prod + '/' + fonts));
+	gulp.src([app + '/*.*', 'node_modules/apache-server-configs/dist/.htaccess'], {
+			dot: true
+		})
+		.pipe(gulp.dest(prod));
 });
 
-gulp.task('build',['clean:prod'],function(cb){
-	runSequence(['styles','scripts','templates','images','copy:prod'],'html', cb);
+gulp.task('build', ['clean:prod'], function(cb) {
+	runSequence(['styles', 'scripts', 'templates', 'images', 'copy:prod'], 'html', cb);
 });
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
 gulp.task('pagespeed', pagespeed.bind(null, {
-  // By default, we use the PageSpeed Insights
-  // free (no API key) tier. You can use a Google
-  // Developer API key if you have one. See
-  // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'http://www.frontendler.com.br',
-  strategy: 'mobile'
+	// By default, we use the PageSpeed Insights
+	// free (no API key) tier. You can use a Google
+	// Developer API key if you have one. See
+	// http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
+	url: 'http://www.frontendler.com.br',
+	strategy: 'mobile'
 }));
