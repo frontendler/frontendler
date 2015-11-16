0fb8d683639032b213f677edd04c90e078b266bd
Daniel Beff <danielbeff@MacBook-Pro-de-Daniel.local>
2015-11-16 21:01:16 -0200

    update sass library

---
+++ app/assets/scripts/main.js
@@ -0,0 +1,10 @@
+/**
+* teste
+*
+* @param {string} param1 - The title of the book.
+* @param {string} param2 - The author of the book.
+* @returns {string}
+*/
+function teste (param1,param2){
+ return "teste";
+};
